d3118eef3602c3d9ad21d37db7b59a82a818b930
gurucharan1027 <74811644+gurucharan1027@users.noreply.github.com>
2025-05-23 13:19:15 +0530

    review update

--- experiment/simulation/js/script2.js
+++ experiment/simulation/js/script2.js
@@ -18,3 +18,75 @@
 		document.getElementById('differentres').style.border="2px solid red";
 		document.getElementById('sameres').style.border="none";
 	}
+	function checkSelection() {
+		let selectBox = document.getElementById("cars");
+		let selectedValue = selectBox.value;
+		let result = document.getElementById("result");
+		
+		if(selectedValue === "null")
+		{
+			alert("Please select the answer");
+		}
+		else if (selectedValue === "blR") {
+			document.getElementById("result").innerHTML = "Answer is Correct. <br> Null Space = {(x, y)&isin;R<sup>2</sup>: x=0} = y-axis";
+			result.style.color = "green";
+		} else {
+			document.getElementById("result").innerHTML = "Answer is Incorrect. <br> Correct answer is Null Space = {(x, y)&isin;R<sup>2</sup>: x=0} = y-axis";
+			result.style.color = "red";
+		}
+	}
+
+	function checkSelection2() {
+		let selectBox = document.getElementById("nullity");
+		let selectedValue = selectBox.value;
+		let result = document.getElementById("result2");
+
+		if(selectedValue === "null")
+		{
+			alert("Please select the answer");
+		}
+		else if (selectedValue === "2" || selectedValue === "1" || selectedValue === "0") {
+			document.getElementById("result2").innerHTML = "Answer is Correct.";
+			result.style.color = "green";
+		} else {
+			document.getElementById("result2").innerHTML = "Answer is Incorrect.";
+			result.style.color = "red";
+		}
+	}
+
+	
+	function selectRank() {
+		let selectBox = document.getElementById("rank");
+		let selectedValue = selectBox.value;
+		let result = document.getElementById("result3");
+
+		if(selectedValue === "null")
+		{
+			alert("Please select the answer");
+		}
+		else if (selectedValue === "1") {
+			document.getElementById("result3").innerHTML = "Answer is Correct.";
+			result.style.color = "green";
+		} else {
+			document.getElementById("result3").innerHTML = "Answer is Incorrect.";
+			result.style.color = "red";
+		}
+	}
+
+	function selectRange() {
+		let selectBox = document.getElementById("range");
+		let selectedValue = selectBox.value;
+		let result = document.getElementById("result4");
+
+		if(selectedValue === "null")
+		{
+			alert("Please select the answer");
+		}
+		else if (selectedValue === "2x") {
+			document.getElementById("result4").innerHTML = "Answer is Correct.";
+			result.style.color = "green";
+		} else {
+			document.getElementById("result4").innerHTML = "Answer is Incorrect.";
+			result.style.color = "red";
+		}
+	}
